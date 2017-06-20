c099c40d10ae7da74ad83e8fc6100685b0f69a86
al-walker <walker.albert@gmail.com>
2017-06-20 10:13:04 -0400

    Add files via upload

--- exercises_ch1.rb
+++ exercises_ch1.rb
@@ -1,5 +1,15 @@
 require 'rubocop'
 
+#compute sum using Gauss formula
+
+def gauss_formula(n)
+  n * (n + 1) / 2
+end
+
+p gauss_formula(100)
+
+# compute sum using loop
+
 def compute_sum(n)
   i = 1
   num = 0
