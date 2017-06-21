b4f23a3ef038e3775272f96aa2394d49e72ee5ee
al-walker <walker.albert@gmail.com>
2017-06-21 12:15:54 -0400

    Add files via upload

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,18 +1,23 @@
 require 'pry'
 
-# Create the functionality for the square button
-def square(n)
-  n * n
+# determine if number is even
+# deduct = 0 if even 1 if odd
+
+def calculate_binary(number)
+  binary = []
+  total = 0
+  deduct = 0
+
+  deduct = number.even? ? 0 : 1
+  total = (number - deduct) / 2
+  binary << deduct
+  loop do
+    deduct = total.even? ? 0 : 1
+    total = (total - deduct) / 2
+    binary << deduct
+    break if total < 1
+  end
+  binary.reverse.join.to_i
 end
 
-p square(2)
-p square(4)
-p square(7)
-
-# Create the functionality for the exp button
-
-def exp(number, exponent)
-  number * (10**exponent)
-end
-
-p exp(7, 4)
+p calculate_binary(208)
