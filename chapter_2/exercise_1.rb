b933c5f2982d4f87720a19d48cbd6623e328a6d1
al-walker <walker.albert@gmail.com>
2017-06-21 12:20:19 -0400

    Add files via upload

--- chapter_2/exercise_1.rb
+++ chapter_2/exercise_1.rb
@@ -1,1 +1,23 @@
+require 'pry'
 
+# determine if number is even
+# deduct = 0 if even 1 if odd
+
+def calculate_binary(number)
+  binary = []
+  total = 0
+  deduct = 0
+
+  deduct = number.even? ? 0 : 1
+  total = (number - deduct) / 2
+  binary << deduct
+  loop do
+    deduct = total.even? ? 0 : 1
+    total = (total - deduct) / 2
+    binary << deduct
+    break if total < 1
+  end
+  binary.reverse.join.to_i
+end
+
+p calculate_binary(208)
