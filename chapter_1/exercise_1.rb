77a60043b62ff665a9725cfd779f2fd5ce7538d2
al-walker <walker.albert@gmail.com>
2017-06-21 12:22:21 -0400

    Add files via upload

--- chapter_1/exercise_1.rb
+++ chapter_1/exercise_1.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+# Create the functionality for the square button
+def square(n)
+  n * n
+end
+
+p square(2)
+p square(4)
+p square(7)
+
+# Create the functionality for the exp button
+
+def exp(number, exponent)
+  number * (10**exponent)
+end
+
+p exp(7, 4)
